7d926dee922c80893a3a2cfc1be08fdcbdf9f3d4
Shivam Kansagara <202001086@daiict.ac.in>
2023-07-07 04:20:51 +0530

    adding pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 
 function App() {
   return (
-    <>
-    <h1>hellofdmbodfooi m</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
